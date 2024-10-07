0dbd0f5c074e11458bd3c0d7f652491f8d26bc67
Mourchidi <mfoumbymourchidi@gmail.com>
2024-10-07 10:01:30 +0200

    Initial Commit

---
+++ gedoise/gedoiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  gedoiseApp.swift
+//  gedoise
+//
+//  Created by Mourchidi Mfoumby on 07/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct gedoiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
