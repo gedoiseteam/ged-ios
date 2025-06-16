f3e8f5efd4046c1c19dc9637643fdbe28065d57e
Mourchidi <mmfoumby@unowhy.com>
2025-06-16 09:54:30 +0200

    refacto: disable firestore persistence

--- LaGrandeEcoleDuDroit/App/LaGrandeEcolueDuDroitApp.swift
+++ LaGrandeEcoleDuDroit/App/LaGrandeEcolueDuDroitApp.swift
@@ -1,5 +1,6 @@
 import SwiftUI
 import FirebaseCore
+import Firebase
 
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(
@@ -7,7 +8,13 @@ class AppDelegate: NSObject, UIApplicationDelegate {
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
     ) -> Bool {
         FirebaseApp.configure()
-//        GedConfiguration.configureDebugFirebase()
+        
+        let db = Firestore.firestore()
+        let settings = FirestoreSettings()
+        
+        settings.cacheSettings = MemoryCacheSettings()
+        db.clearPersistence()
+        db.settings = settings
         return true
     }
 }
